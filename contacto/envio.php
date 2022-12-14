a2ab3b85199fc008d8834c18eb00e9037bee6630
alejodev2103 <alejod2103@gmail.com>
2022-12-14 20:15:08 +0000

    primer commit

---
+++ contacto/envio.php
@@ -0,0 +1,18 @@
+<?php
+
+$destino = "alejod2103@gmail.com";
+//Esto es al correo que se enviara el mail
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];  
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+$header = "Enviado desde formulario en INTERCOMM-SERVICES";
+$mensajeCompleto = "\n Nombre: " . $nombre . "\n Apellido: ". $apellido . "\n" .  "Email: " . $email . "\n" . "Télefono :" . $telefono . "\n". "Mensaje :" . $mensaje;
+mail($destino, $asunto, $mensajeCompleto, $header);
+header('Location: index.html');
+
+?>
